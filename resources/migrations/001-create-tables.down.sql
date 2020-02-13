a2a483eb9363cfca099e48bf1364a107b91ac8f5
Mikhail Mikhailov <mikhail.mikhailov@takeoff.com>
2020-02-13 16:23:06 -0500

    changed the order of tables being dropped.

--- resources/migrations/001-create-tables.down.sql
+++ resources/migrations/001-create-tables.down.sql
@@ -1,5 +1,5 @@
-DROP TABLE users;
-DROP TABLE roles;
-DROP TABLE permissions;
-DROP TABLE user_roles;
 DROP TABLE role_permissions;
+DROP TABLE user_roles;
+DROP TABLE permissions;
+DROP TABLE roles;
+DROP TABLE users;
