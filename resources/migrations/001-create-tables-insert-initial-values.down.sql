ec7c7d3f7d7064379f3c67f490e33a99d423a534
Mikhail Mikhailov <mikhail.mikhailov@takeoff.com>
2020-02-13 16:16:02 -0500

    added resources folder.

---
+++ resources/migrations/001-create-tables-insert-initial-values.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE role_permissions;
+DROP TABLE user_roles;
+DROP TABLE permissions;
+DROP TABLE roles;
+DROP TABLE users;
---
+++ resources/migrations/001-create-tables.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE users;
+DROP TABLE roles;
+DROP TABLE permissions;
+DROP TABLE user_roles;
+DROP TABLE role_permissions;
